327293cf837f5fa9af8748084f414190ac72644f
Weronika <weronika_dabrowska@yahoo.co.uk>
2025-06-15 19:26:38 +0100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Some say the world will end in fire",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
